6b55e9e3468a4b9f7df763b39121ffca16a9d345
Pranav-Bobde <bobdep31@gmail.com>
2022-10-25 23:26:19 +0530

    added pinia

---
+++ src/pages/FormsPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { useUserStore } from "stores/user";
+
+const store = useUserStore();
+let data = ref(null);
+onMounted(() => {
+  data.value = store.getUser;
+});
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -36,22 +36,26 @@ export default defineComponent({
 
 <script setup>
 import { ref, watch } from "vue";
+import { useRouter } from "vue-router";
+import { useUserStore } from "stores/user";
+
+const store = useUserStore();
+const router = useRouter();
 const pollData = ref(null);
 let msg = ref(null);
 let data = ref(null);
 
 const handleClick = () => {
   window.location = `truecallersdk://truesdk/web_verify?requestNonce=123123123&partnerKey=fq3Sbba1003078b504910ae04fc3f59560c09&partnerName=KoffeLaDo&lang=en&title=continue`;
-
   pollData.value = setInterval(async () => {
     let requestNonce = 123123123;
     let res = await fetch(
       `https://true-be.herokuapp.com/api/getuser/${requestNonce}`
-      // `http://localhost:5001/api/getuser/${requestNonce}`
     );
     res = await res.json();
     console.log(res);
     data.value = res.user;
+    store.setUser(res.user);
     msg.value = res.msg;
     console.log("data: ", data);
     console.log("msg: ", msg.value);
@@ -63,6 +67,7 @@ watch(
   (newMsg, oldMsg) => {
     if (newMsg === "Success") {
       clearInterval(pollData.value);
+      router.push({ path: "/forms" });
     }
   }
 );
