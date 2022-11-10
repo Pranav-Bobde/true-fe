78727c22b66e8104cfd1d486ae8d5453ef44bb8a
Pranav-Bobde <bobdep31@gmail.com>
2022-11-10 22:43:13 +0530

    corrected routes, added lintrc, updated gitignore

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,11 +2,13 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
-    children: [{ path: "/menu", component: () => import("pages/MenuPage.vue") }],
-    children: [{ path: "/order", component: () => import("pages/OrderPage.vue") }],
-    children: [{ path: "/feedback", component: () => import("pages/FeedbackPage.vue") }],
-    children: [{ path: "/thankyou", component: () => import("pages/ThankYouPage.vue") }],
+    children: [
+      { path: "", component: () => import("pages/IndexPage.vue") },
+      { path: "/menu", component: () => import("pages/MenuPage.vue") },
+      { path: "/order", component: () => import("pages/OrderPage.vue") },
+      { path: "/feedback", component: () => import("pages/FeedbackPage.vue") },
+      { path: "/thankyou", component: () => import("pages/ThankYouPage.vue") },
+    ],
   },
 
   // Always leave this as last one,
