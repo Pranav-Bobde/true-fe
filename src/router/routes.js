5267f13efe86b59930c29dba352c1f0545e8bed7
Pranav-Bobde <yagyeshSRbobde@gmail.com>
2022-11-10 15:25:51 +0530

    base app

---
+++ src/router/routes.js
@@ -0,0 +1,20 @@
+const routes = [
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+    children: [{ path: "/menu", component: () => import("pages/MenuPage.vue") }],
+    children: [{ path: "/order", component: () => import("pages/OrderPage.vue") }],
+    children: [{ path: "/feedback", component: () => import("pages/FeedbackPage.vue") }],
+    children: [{ path: "/thankyou", component: () => import("pages/ThankYouPage.vue") }],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
+
+export default routes;
