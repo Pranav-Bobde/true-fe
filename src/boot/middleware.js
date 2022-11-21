2d0a3cad303375c9ed39f85be7dae69e27703b73
Pranav-Bobde <bobdep31@gmail.com>
2022-11-21 18:04:18 +0530

    added status bases routing

--- src/boot/middleware.js
+++ src/boot/middleware.js
@@ -1,24 +1,52 @@
-import { boot, route } from "quasar/wrappers";
+import { boot } from "quasar/wrappers";
 import { useUserStore } from "src/stores/user-store";
 
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(async ({ router, store }) => {
   const userStore = useUserStore(store);
-  // console.log("store: ", userStore);
-  // console.log("id: ", userStore.user.id)
-  const id = window.localStorage.getItem("id");
+  
   router.beforeEach((to, from, next) => {
-    // console.log("from: ", from);
-    // console.log("to: ", to);
-    if (id) {
-      next()
-    } else {
+    const user_id = userStore.user.id || window.localStorage.getItem("id") || null;
+    const ls_user_status = window.localStorage.getItem("user_status");
+
+    if(!user_id){
+      console.log("first")
       if (to.path === "/") {
         next();
       } else {
         next("/");
       }
+
+    }else {
+
+      console.log("Second")
+      console.log(ls_user_status) 
+      console.log(process.env.STATUS_USER_CONFIRMED)
+
+      if(ls_user_status == process.env.STATUS_USER_CONFIRMED){
+        console.log("USER CONFIRMED")
+        if (to.path === "/order") {
+          next();
+        } else {
+          router.push("/order");
+        }
+
+      }else if (ls_user_status == process.env.STATUS_FEEDBACK_LEFT) {
+        console.log("FEEDBACK LEFT")
+        if (to.path === "/feedback") {
+          next();
+        } else {
+          router.push("/feedback");
+        }
+
+      } else if (ls_user_status == process.env.STATUS_FEEDBACK_COMPLETE) {
+        console.log("FEEDBACK COMPLETE")
+        if (to.path === "/thankyou") {
+          next();
+        } else {
+          router.push("/thankyou");
+        }
+      }
+
     }
   });
 });
