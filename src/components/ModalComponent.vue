495952834bc7d8afde466a2e9b6fd565937f9903
Pranav-Bobde <bobdep31@gmail.com>
2022-11-21 18:01:43 +0530

    updated Modal Component

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -1,33 +1,112 @@
 <!-- eslint-disable vue/no-mutating-props -->
 <template>
-  <q-dialog v-model="store.modal.show">
+  <q-dialog v-model="menuStore.modal.show">
     <q-card class="modal">
       <q-card-section class="row text-center items-center">
-        <span class="text q-ml-sm">Confirm {{ store.modal.title }} ?</span>
+        <span class="text q-ml-sm">Confirm {{ menuStore.modal.title }} ?</span>
       </q-card-section>
 
       <q-card-actions class="btn-group">
-        <ButtonComponent @click="store.closeModal" :isDarkBG="false"
-          >NO</ButtonComponent
-        >
-        <ButtonComponent @click="handleClick" :isDarkBG="true"
-          >YES</ButtonComponent
-        >
+        <ButtonComponent @click="menuStore.closeModal" :isDarkBG="false">
+          NO
+        </ButtonComponent>
+        <ButtonComponent @click="handleClick" :isDarkBG="true">
+          YES
+        </ButtonComponent>
       </q-card-actions>
     </q-card>
   </q-dialog>
 </template>
 
 <script setup>
-import ButtonComponent from "./ButtonComponent.vue";
+import { ref, watch } from "vue";
 import { useMenuStore } from "../stores/menuStore";
+import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "vue-router";
+import { useUserStore } from "src/stores/user-store";
+import ButtonComponent from "./ButtonComponent.vue";
+import { updateUserStatus } from "src/functions/updateUsetStatus";
 
-const store = useMenuStore();
+const menuStore = useMenuStore();
+const userStore = useUserStore();
+const interval = ref(null);
+const router = useRouter();
+const user = ref(null);
+const order = ref(null);
 
 function handleClick() {
-  store.addToCart(store.modal.id);
-  store.closeModal();
-  // TODO - Add logic for truecaller login and post order object to aws.
+  menuStore.addToCart(menuStore.modal.id);
+  menuStore.closeModal();
+  const is_IOS =
+    /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  if (is_IOS) {
+    // Manual
+  } else {
+    order.value = {
+      id: uuidv4(),
+      order: menuStore.order,
+      total: menuStore.total,
+      time: new Date().toLocaleString(),
+    };
+
+    // console.log("Order: ", order);
+
+    // window.location = `truecallersdk://truesdk/web_verify?requestNonce=${order.id}&partnerKey=fq3Sbba1003078b504910ae04fc3f59560c09&partnerName=KoffeLaDo&lang=en&title=continue`;
+
+    interval.value = setInterval(async () => {
+      let res = await fetch(process.env.USER_API + "/1", {
+        method: "GET",
+        headers: {
+          "x-api-key": process.env.KEY,
+        },
+      });
+
+      res = await res.json();
+      console.log("Res: ", res);
+      user.value = res.user;
+      console.log("UserValue: ", user.value);
+      const { id, name, phno } = user.value;
+      userStore.setUser({ id, name, phno });
+      console.log("storeUser: ", userStore.getUser);
+      clearInterval(interval.value);
+    }, 1500);
+  }
+
+  watch(
+    () => user.value,
+    async (newUser, oldUser) => {
+      if (newUser) {
+        let updatedStatus = await updateUserStatus(
+          user.value.id,
+          process.env.STATUS_USER_CONFIRMED
+        );
+
+        console.log("ModalUpdatedStatus: ", updatedStatus);
+
+        window.localStorage.setItem("id", user.value.id);
+        window.localStorage.setItem(
+          "user_status",
+          statusUpdated["updated"]["Attributes"]["user_status"]
+        );
+
+        console.log(
+          "lsId,status: ",
+          window.localStorage.getItem("id"),
+          window.localStorage.getItem("user_status")
+        );
+
+        clearInterval(interval.value);
+
+        // TODO -  post order to db
+
+        if (updatedStatus) {
+          router.push({ path: "/order" });
+        } else {
+          // TODO - Handle error
+        }
+      }
+    }
+  );
 }
 </script>
 
@@ -43,6 +122,4 @@ function handleClick() {
 .btn-group {
   justify-content: space-evenly;
 }
-/* .modal-title {
-} */
 </style>
