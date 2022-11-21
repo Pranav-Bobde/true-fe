abd64654ffb95a042999c21251845dd792139d20
silky1611 <silkyag1611@gmail.com>
2022-11-21 22:48:43 +0530

    added thankyou page

--- src/pages/ThankyouPage.vue
+++ src/pages/ThankyouPage.vue
@@ -1,7 +1,15 @@
 <template>
-  <q-page padding>
-    <!-- content -->
-    <h5>Thankyou</h5>
+  <q-page class="main-page flex flex-center">
+      
+    <img
+        src="../assets/kld-logo.png"
+        alt="logo"
+        class="kld-img text-center"
+      >
+    <div class="text-center q-px-xl text text-dark">
+      Thank you for your time and the valuable response!!
+    </div>
+
   </q-page>
 </template>
 
@@ -10,3 +18,14 @@ export default {
   // name: 'PageName',
 }
 </script>
+<style lang="scss" scoped>
+.main-page {
+  background-color: $primary;
+  flex-direction: column ;
+  // gap: 2rem;
+}
+.kld-img {
+  height: 25vh;
+  padding: 1rem;
+}
+</style>
